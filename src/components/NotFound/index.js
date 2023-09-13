f21618ee37ee50731acc6df1ca7a9d1ef588f64c
swathi <uttaralaswathi1998@gmail.com>
2023-09-13 11:58:27 +0000

    40A

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -14,7 +14,7 @@ const NotFound = () => (
         />
         <h1 className="heading">Lost Your Way?</h1>
         <p className="description">
-          Sorry, we cannot find that page. You will find lot to explore on the
+          Sorry, we cannot find that page. You will find lots to explore on the
           home page.
         </p>
       </div>
